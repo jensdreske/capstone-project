151b1459b74e817525247eaeb284ab8d2d0dca29
jens <jdreske@web.de>
2021-06-28 21:07:29 +0200

    changed typo in goalSchema

--- server/models/goal.model.js
+++ server/models/goal.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const goalSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
-  co2InKgPerUnit: { type: Number, required: false },
+  co2InKgPerUnit: { type: Number, required: true },
   image: String,
   userName: String,
   verified: Boolean,
