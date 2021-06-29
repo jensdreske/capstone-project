f8c62efb9e2d6f6e6ada151e4895e42a2f94445e
jens <jdreske@web.de>
2021-06-29 19:27:28 +0200

    fixed community goals calculation

--- client/src/components/CommunityGoals.js
+++ client/src/components/CommunityGoals.js
@@ -87,7 +87,6 @@ export default function CommunityGoals({
                 onClick={() => {
                   toggleEmissionsFromCommunityGoals(
                     goal,
-                    goals,
                     emissionsFromGoals,
                     setEmissionsFromGoals
                   );
